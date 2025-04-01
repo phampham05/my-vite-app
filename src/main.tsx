d5b01cf529ac6260ab7ad70450f69272a9e42b32
PP <euyu@192.168.20.110>
2025-04-01 21:34:35 +0700

    Kiem tra sau thay doi

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.tsx'
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.css'  // Import file CSS
+import App from './App.tsx'  // Import App component
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App />  {/* Render component App */}
   </StrictMode>,
 )
