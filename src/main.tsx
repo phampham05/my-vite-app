cc6a1b399b91ef4e332392b95713c57710a65e98
PP <euyu@192.168.20.110>
2025-04-01 21:42:58 +0700

    Kiem tra

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,13 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.tsx'
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'  // Import file CSS
-import App from './App.tsx'  // Import App component
+import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />  {/* Render component App */}
+    <App />
   </StrictMode>,
 )
+
+
+
